c2ab64b72054de82f7916ac74d07357f9385a0cb
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2017-10-01 14:27:20 +0900

    Update SYNOPSIS

--- lib/App/Prove/Plugin/CumulativeTimer.pm
+++ lib/App/Prove/Plugin/CumulativeTimer.pm
@@ -39,10 +39,16 @@ App::Prove::Plugin::CumulativeTimer - A prove plugin to display cumulative elaps
 
 =head1 SYNOPSIS
 
-    $ prove -PCumulativeTimer
+    $ prove -PCumulativeTimer tests
 
-    # 
-    # ... 
+    # [14:22:52] tests/test1.t .. ok     2052 ms ( 0.00 usr  0.00 sys +  0.04 cusr  0.01 csys =  0.05 CPU)
+    # [14:22:54] tests/test2.t .. ok     2111 ms ( 0.01 usr  0.00 sys +  0.08 cusr  0.02 csys =  0.11 CPU)
+
+    # When you don't use this plugin, elapsed time of tests/tes2.t is not cumulative.
+    $ prove --timer tests
+
+    # [14:22:31] tests/test1.t .. ok     2049 ms ( 0.00 usr  0.00 sys +  0.04 cusr  0.01 csys =  0.05 CPU)
+    # [14:22:33] tests/test2.t .. ok       60 ms ( 0.01 usr  0.00 sys +  0.05 cusr  0.01 csys =  0.07 CPU)
 
 =head1 DESCRIPTION
 
