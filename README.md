5f1efa50f5823b7dba57047ee7950d5953506381
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2017-10-01 14:15:16 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# NAME
+
+App::Prove::Plugin::CumulativeTimer - A prove plugin to display cumulative elapsed time of tests.
+
+# SYNOPSIS
+
+    $ prove -PCumulativeTimer
+
+    # 
+    # ... 
+
+# DESCRIPTION
+
+App::Prove::Plugin::CumulativeTimer is a prove plugin to display cumulative elapsed time of tests.
+
+This plugin replaces elaped time of --timer option with cumulative elapsed time.
+
+\--timer option is always set when you load this plugin.
+
+# LICENSE
+
+Copyright (C) Masahiro Iuchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Masahiro Iuchi <masahiro.iuchi@gmail.com>
---
+++ lib/App/Prove/Plugin/CumulativeTimer.pm
@@ -0,0 +1,67 @@
+package App::Prove::Plugin::CumulativeTimer;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+use Class::Method::Modifiers qw( around );
+use TAP::Parser;
+use TAP::Formatter::Session;
+
+sub load {
+    my ( $class, $p ) = @_;
+
+    $p->{app_prove}->timer(1);
+
+    my $first_start_time;
+    my $first_start_times;
+    around 'TAP::Formatter::Session::time_report' => sub {
+        my $orig = shift;
+        my ( $self, $formatter, $parser ) = @_;
+        $first_start_time  ||= $parser->start_time;
+        $first_start_times ||= $parser->start_times;
+        no warnings 'redefine';
+        local *TAP::Parser::start_time  = sub {$first_start_time};
+        local *TAP::Parser::start_times = sub {$first_start_times};
+        return $orig->(@_);
+    };
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Prove::Plugin::CumulativeTimer - A prove plugin to display cumulative elapsed time of tests.
+
+=head1 SYNOPSIS
+
+    $ prove -PCumulativeTimer
+
+    # 
+    # ... 
+
+=head1 DESCRIPTION
+
+App::Prove::Plugin::CumulativeTimer is a prove plugin to display cumulative elapsed time of tests.
+
+This plugin replaces elaped time of --timer option with cumulative elapsed time.
+
+--timer option is always set when you load this plugin.
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Iuchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Masahiro Iuchi E<lt>masahiro.iuchi@gmail.comE<gt>
+
+=cut
+
